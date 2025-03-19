631bece62bed1e4c5d043c0d830bbc0a1f587977
BekaChkhiro <bekachkhirodze1@gmail.com>
2025-03-19 12:15:51 +0400

    Auction item without buy now button

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '55226f9a8167df3b4272');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'f0ca171349f98edd7ab6');
--- src/pages/AuctionArchive/components/AuctionItem.jsx
+++ src/pages/AuctionArchive/components/AuctionItem.jsx
@@ -108,13 +108,6 @@ const AuctionItem = ({
           >
             {texts.placeBid}
           </Link>
-          <Link
-            to={getAuctionLink(auction.id)}  
-            className="w-full py-2 sm:py-3 text-center text-sm sm:text-base rounded-full"
-            style={{ backgroundColor: '#E6E6E6' }}
-          >
-            {texts.buyNow} {auction.meta.buy_now}{texts.currency}
-          </Link>
         </div>
       </Link>
     </div>
